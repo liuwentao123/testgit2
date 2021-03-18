46e4231808faf5ab936bb6dc25b01ae83d2661b9
liuwentao <liu.wentao@immomo.com>
2021-03-18 10:49:34 +0800

    1

---
+++ src/main/java/com/lwt/testgit2/Test.java
@@ -0,0 +1,11 @@
+package com.lwt.testgit2;
+
+/**
+ * Created by liu.wentao on 2021-03-18 10:48
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
---
+++ src/test/java/com/lwt/testgit2/Testgit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwt.testgit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testgit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
